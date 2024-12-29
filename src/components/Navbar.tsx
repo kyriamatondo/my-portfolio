217f26424cd5d041a0eaf4535b6fa2fa40cd1df5
kyriamatondo <krmatondo@gmail.com>
2024-12-29 10:59:56 +0100

    ajout de fixed sur la navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,15 +2,15 @@ import { Laptop } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-center md:justify-between items-center p-4">
+    <div className="md:fixed md:top-0 left-0 w-full md:bg-secondary z-10 flex justify-center md:justify-between items-center p-4">
       <a href="#" className="flex items-center font-bold text-3xl md:text-xl">
         <Laptop className="mr-2" />
-        KY<span className="text-secondary">DEV</span>
+        KY<span className="text-primary">DEV</span>
       </a>
 
-      <ul className="hidden md:flex space-x-4">
+      <ul className="hidden md:flex space-x-4 text-white">
         <li>
-          <a href="#Home" className="btn btn-sm btn-ghost">
+          <a href="#Home" className="btn btn-sm btn-ghost ">
             Accueil
           </a>
         </li>
